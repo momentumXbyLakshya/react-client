2dc6237985a40a32b9d4d380d6faad044f09078d
Lakshya Satpal <lakshyasatpal372@gmail.com>
2023-10-31 16:50:01 +0530

    token in header

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-axios.defaults.withCredentials = true;
-
 export const axiosInstance = axios.create({
   baseURL: `${process.env.REACT_APP_BACKEND_URI}/api`,
   timeout: 20000,
