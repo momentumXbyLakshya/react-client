75f0cabfcb61a7e96ad35ebad5bc70b74ba56eaf
Lakshya Satpal <lakshyasatpal372@gmail.com>
2023-10-31 17:53:14 +0530

    cookies used

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,12 +1,9 @@
 import axios from "axios";
-import Cookies from "js-cookie";
+
+axios.defaults.withCredentials = true;
 
 export const axiosInstance = axios.create({
   baseURL: `${process.env.REACT_APP_BACKEND_URI}/api`,
+  withCredentials: true,
   timeout: 20000,
-  headers: {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Credentials": true,
-    Authorization: `Bearer ${Cookies.get("hanko")}`,
-  },
 });
