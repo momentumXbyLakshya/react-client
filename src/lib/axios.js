9f3a1bd34b771e2f10a86d68c5867312ce65227e
Lakshya Satpal <lakshyasatpal372@gmail.com>
2023-10-31 16:40:03 +0530

    final commit

---
+++ src/lib/axios.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+axios.defaults.withCredentials = true;
+
+export const axiosInstance = axios.create({
+  baseURL: `${process.env.REACT_APP_BACKEND_URI}/api`,
+  timeout: 20000,
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Credentials": true,
+    Authorization: `Bearer ${Cookies.get("hanko")}`,
+  },
+});
