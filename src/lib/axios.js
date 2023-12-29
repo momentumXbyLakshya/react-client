9fef5d3e335b5a5fb90cf7e3025c3e5db0d5d3be
Lakshya Satpal <lakshyasatpal372@gmail.com>
2023-12-30 00:56:30 +0530

    Added Auth header along with cookies

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -7,3 +7,32 @@ export const axiosInstance = axios.create({
   withCredentials: true,
   timeout: 20000,
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = getAuthToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+function getAuthToken() {
+  const tokenName = "hanko";
+  const cookies = document.cookie.split(";");
+
+  for (let i = 0; i < cookies.length; i++) {
+    let cookie = cookies[i];
+    while (cookie.charAt(0) === " ") {
+      cookie = cookie.substring(1, cookie.length);
+    }
+    if (cookie.indexOf(tokenName + "=") === 0) {
+      return cookie.substring(tokenName.length + 1, cookie.length);
+    }
+  }
+  return null;
+}
