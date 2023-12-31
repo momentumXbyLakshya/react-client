3de3307cf70c54d7995395f72db96c76d522a9cd
Lakshya Satpal <lakshyasatpal372@gmail.com>
2023-12-31 18:12:56 +0530

    removed credentials from axios

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-axios.defaults.withCredentials = true;
+// axios.defaults.withCredentials = true;
 
 export const axiosInstance = axios.create({
   baseURL: `${process.env.REACT_APP_BACKEND_URI}/api`,
-  withCredentials: true,
+  // withCredentials: true,
   timeout: 20000,
 });
 
